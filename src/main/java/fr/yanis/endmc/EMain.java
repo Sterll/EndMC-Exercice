0e3e77fa4868a1853fc0d1131808a7ed07c173d3
Yanis - Sterll <yanis.benyacine@ynov.com>
2025-03-09 17:36:03 +0100

    Initialize RingManager in EMain and add instance retrieval methods

--- src/main/java/fr/yanis/endmc/EMain.java
+++ src/main/java/fr/yanis/endmc/EMain.java
@@ -4,14 +4,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EMain extends JavaPlugin {
 
+    private RingManager ringManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.ringManager = new RingManager();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static EMain getInstance() {
+        return EMain.getPlugin(EMain.class);
+    }
+
+    public RingManager getRingManager() {
+        return ringManager;
     }
 }

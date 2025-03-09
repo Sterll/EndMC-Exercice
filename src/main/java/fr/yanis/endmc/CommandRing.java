3d4b64009749921874fce0585b4c2a00ba521abe
Yanis - Sterll <yanis.benyacine@ynov.com>
2025-03-09 19:38:19 +0100

    Implement tab completion for ring commands in CommandRing

--- src/main/java/fr/yanis/endmc/CommandRing.java
+++ src/main/java/fr/yanis/endmc/CommandRing.java
@@ -4,10 +4,15 @@ import fr.yanis.endmc.ring.IRing;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class CommandRing implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandRing implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
@@ -50,5 +55,24 @@ public class CommandRing implements CommandExecutor {
         }
 
         return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
+        List<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+            completions.add("enable");
+            completions.add("disable");
+            return completions;
+        } else if (args.length == 2) {
+            EMain.getInstance().getRingManager().getRings().keySet().forEach(ring -> {
+                if (ring.startsWith(args[1])) {
+                    completions.add(ring);
+                }
+            });
+        }
+
+        return List.of();
     }
 }

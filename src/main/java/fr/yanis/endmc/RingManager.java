46a8dc3e68c5778b318f0f2426e888dd9c32d96b
Yanis - Sterll <yanis.benyacine@ynov.com>
2025-03-09 17:33:23 +0100

    Add RingManager class to manage ring functionality and player interactions

---
+++ src/main/java/fr/yanis/endmc/RingManager.java
@@ -0,0 +1,47 @@
+package fr.yanis.endmc;
+
+import fr.yanis.endmc.ring.IRing;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class RingManager {
+
+    private HashMap<String, IRing> rings = new HashMap<>();
+
+    public void registerRing(IRing ring) {
+        rings.put(ring.getIdentifier(), ring);
+    }
+
+    public IRing getRing(String identifier) {
+        return rings.get(identifier);
+    }
+
+    public void enableRing(String identifier, Player player) {
+        IRing ring = getRing(identifier);
+        if (ring != null) {
+            ring.onEnable();
+            ring.setEnabled(true);
+            ring.setPlayer(player);
+        }
+    }
+
+    public void disableRing(String identifier) {
+        IRing ring = getRing(identifier);
+        if (ring != null) {
+            ring.onDisable();
+            ring.setEnabled(false);
+            ring.setPlayer(null);
+        }
+    }
+
+    public void tickRings() {
+        rings.values().forEach(ring -> {
+            if (ring.isEnabled()) {
+                ring.onTick();
+            }
+        });
+    }
+
+}
